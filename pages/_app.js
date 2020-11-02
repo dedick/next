71dc2f8a49d7133e6696e564fc3e7611ad94286d
Remi Dedicker <remi@tappx.com>
2020-11-02 15:54:33 +0100

    add postcss and tailwind config file

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 import '../styles/globals.css'
 
+export function reportWebVitals(metric) {
+  console.log(metric)
+}
+
 function MyApp({ Component, pageProps }) {
+  console.log('myApp')
   return <Component {...pageProps} />
 }
 
